f5d36a0c0801710563af5086c4daef5029365811
bipsh <a.dr.99@mail.ru>
2024-11-13 10:02:40 +0500

    function2
    exponent

--- testTwo.js
+++ testTwo.js
@@ -5,3 +5,8 @@ function add(a, b) {
 }
 let summary = add(number, number);
 console.log(summary);
+function exponent(a, b, c) {
+    return (a + b) ** c;
+}
+let exponentResult = exponent(number, number, number);
+console.log(exponentResult);
