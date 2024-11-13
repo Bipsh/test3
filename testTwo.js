2851de988b2b12f0aea9daf1264eead7343797d2
bipsh <a.dr.99@mail.ru>
2024-11-13 09:57:18 +0500

    function
    summary

--- testTwo.js
+++ testTwo.js
@@ -1,4 +1,7 @@
 console.log("Hello World");
 let number = 2;
-let number2 = 2 + 2;
-console.log(number2);
+function add(a, b) {
+    return a + b;
+}
+let summary = add(number, number);
+console.log(summary);
